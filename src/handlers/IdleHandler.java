ead7760a34b38a17426a010dd84fbefd4f0ea62f
amrsa <amr@sort6.com>
2018-04-08 01:46:44 +0200

    pushing new files

---
+++ src/handlers/IdleHandler.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package handlers;
+
+import pvm.PVM;
+
+/**
+ *
+ * @author amrsa
+ */
+public class IdleHandler {
+     private PVM p ;
+
+    public IdleHandler(PVM p) {
+        this.p = p;
+    }
+ 
+ public void update(){
+    
+} 
+}
---
+++ src/handlers/PutToppingsHandler.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package handlers;
+
+import pvm.PVM;
+
+/**
+ *
+ * @author amrsa
+ */
+public class PutToppingsHandler {
+     private PVM p ;
+
+    public PutToppingsHandler(PVM p) {
+        this.p = p;
+    }
+ 
+ public void update(){
+    
+} 
+}
