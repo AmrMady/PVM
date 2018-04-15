7770a6523320e594c08ad77dfef71068f6f94312
amrsa <amr@sort6.com>
2018-04-15 20:18:36 +0200

    pushing GUI files

--- src/GUIs/BakingAndServing.java
+++ src/GUIs/BakingAndServing.java
@@ -5,33 +5,64 @@
  */
 package GUIs;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JProgressBar;
+import javax.swing.JTextField;
 import javax.swing.Timer;
-public class BakingAndServing {
-     static JProgressBar pb = new JProgressBar();
-  static int progress;
-  public static void main(String[] args) {
-    JFrame f = new JFrame();
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f.add(pb);
-    f.pack();
-    f.setVisible(true);
 
-    Timer timer = new Timer(50, new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        progress += 1;
-        if (progress >= 100) {
-          progress = 100;
-          ((Timer) e.getSource()).stop();
-        }
-        pb.setValue(progress);
-      }
-    });
-    timer.start();
-  }
+public class BakingAndServing {
+
+    static JProgressBar pb = new JProgressBar();
+    static int progress;
+
     
+    
+        public BakingAndServing(){
+        JFrame frame = new JFrame();
+        JTextField t1, t2;
+        Boolean check = false;
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        t1 = new JTextField("Welcome to Javatpoint.");
+
+        t1.setBounds(50, 100, 200, 30);
+        pb.setBounds(50, 100, 200, 30);
+        //f.add(t1);
+        //f.add(pb);
+
+        t2 = new JTextField("kosomk");
+        t2.setVisible(false);
+        Timer timer = new Timer(50, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                progress += 1;
+                if (progress >= 100) {
+                    progress = 100;
+                    ((Timer) e.getSource()).stop();
+                }
+                pb.setValue(progress);
+                    if (progress == 100) {
+                    t2.setVisible(true);
+                }
+            }
+        });
+
+        frame.add(t1, BorderLayout.NORTH);
+        frame.add(t2, BorderLayout.SOUTH);
+        frame.add(pb, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+        timer.start();
+    }
+
+
+
+    public static void main(String[] args) {
+       new BakingAndServing();
+    }
+
+
+
 }
--- src/pvm/Demo.java
+++ src/pvm/Demo.java
@@ -12,39 +12,37 @@ import java.util.ArrayList;
  * @author ELECTRO
  */
 public class Demo {
-    
-      public static void main(String[] args) {
-    Maintainer main = Maintainer.Get_Instance() ;
-    main.setID(1234);
-          Topping topping1 = new Topping("tomato", 5);
-          Topping topping2 = new Topping("pepper ", 5);
-    Pizza pizza1 = new Pizza(9,"pepperoni");
-    Pizza pizza2 = new Pizza(8, "seafood");
-    Pizza pizza3 = new Pizza(5, "ff");
-           pizza1.addTopping(topping2);
-           pizza2.addTopping(topping1);
-           
-           Menu menu = new Menu();
-           menu = main.additem(menu, pizza2);
-           menu = main.additem(menu, pizza1);
-           menu = main.Deleteiteam(menu, pizza2);
-           menu = main.UpdateMennu(menu, pizza3, 0);
-           menu.displayPizza();
-           
-           Topping t1 = new Topping("tomato", 50);
-           
-           Inventory inv = new Inventory();
-           inv.setCash(500);
-           inv.setPaste(50);
-           inv.intializeToppings(t1);
-            
-           
-            ArrayList<Pizza> pizzaslist = new ArrayList() ; 
-            pizzaslist.add(pizza2);
-           
-           inv.moveFromInventory(pizzaslist);
-           
+
+    public static void main(String[] args) {
+        Maintainer main = Maintainer.Get_Instance();
+        main.setID(1234);
+        Topping topping1 = new Topping("tomato", 5);
+        Topping topping2 = new Topping("pepper ", 5);
+        Pizza pizza1 = new Pizza(9, "pepperoni");
+        Pizza pizza2 = new Pizza(8, "seafood");
+        Pizza pizza3 = new Pizza(5, "ff");
+        pizza1.addTopping(topping2);
+        pizza2.addTopping(topping1);
+
+        Menu menu = new Menu();
+        menu = main.additem(menu, pizza2);
+        menu = main.additem(menu, pizza1);
+        menu = main.Deleteiteam(menu, pizza2);
+        menu = main.UpdateMennu(menu, pizza3, 0);
+        menu.displayPizza();
+
+        Topping t1 = new Topping("tomato", 50);
+
+        Inventory inv = new Inventory();
+        inv.setCash(500);
+        inv.setPaste(50);
+        inv.intializeToppings(t1);
+
+        ArrayList<Pizza> pizzaslist = new ArrayList();
+        pizzaslist.add(pizza2);
+
+        inv.moveFromInventory(pizzaslist);
+
     }
-    
-    
+
 }
